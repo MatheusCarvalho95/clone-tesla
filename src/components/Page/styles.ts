ddacf5413fbf0a5ea922504aa6f3031cd0cad340
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2020-10-26 19:20:09 -0300

    Done

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+.colored:nth-child(1) {
+background: #f1ffe7;
+/* background-image: url(https://images.unsplash.com/photo-1553440569-bcc63803a83d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1011&q=80);
+background-repeat: no-repeat; */
+}
+.colored:nth-child(2) {
+background: #dfffd9;
+}
+.colored:nth-child(3) {
+background: #cdfeca;
+}
+.colored:nth-child(4) {
+background: #bbfebb;
+}
+.colored:nth-child(5) {
+background: #a9fdac;
+}
+.colored:nth-child(6) {
+background: #90f29c;
+}
+.colored:nth-child(7) {
+background: #77e68c;
+}
+`;
+
+export const Spacer = styled.div `
+height: 15vh;
+background-color: #77e68c;
+`
